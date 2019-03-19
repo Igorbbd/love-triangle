132e1fa71fdbd1405b10f3a8ae6b9f14acec79b5
igorbbd <igorbbd30@gmail.com>
2019-03-19 23:32:48 +0300

    feat: add while loop for love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,16 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  let count = 0; 
+  let i = 0;
+  while (i < preferences.length) {
+    let a = preferences[i] - 1;
+    let b = preferences[a] - 1;
+    let c = preferences[b] - 1;
+     if (c == i){
+       count ++;  
+     }
+    i ++;
+  }
+  return (Math.floor(count / 3));
+}; 
